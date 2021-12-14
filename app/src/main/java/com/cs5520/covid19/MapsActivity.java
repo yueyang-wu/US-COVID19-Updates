b35b5329371443d5edaf1ed05db43f957bc605a9
Yueyang Wu <yueyangwu983@gmail.com>
2021-12-13 21:46:51 -0800

    add google map

--- app/src/main/java/com/cs5520/covid19/MapsActivity.java
+++ app/src/main/java/com/cs5520/covid19/MapsActivity.java
@@ -1,15 +1,8 @@
 package com.cs5520.covid19;
 
+import androidx.fragment.app.FragmentActivity;
+
 import android.os.Bundle;
-import android.widget.Button;
-
-import androidx.appcompat.app.AppCompatActivity;
-import com.google.android.gms.maps.CameraUpdateFactory;
-import com.google.android.gms.maps.GoogleMap;
-import com.google.android.gms.maps.OnMapReadyCallback;
-import com.google.android.gms.maps.SupportMapFragment;
-import com.google.android.gms.maps.model.LatLng;
-import com.google.android.gms.maps.model.MarkerOptions;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
@@ -17,37 +10,24 @@ import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.cs5520.covid19.databinding.ActivityMapsBinding;
 
-import org.json.JSONArray;
+public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
 
-public class VaccinationFinder extends AppCompatActivity implements OnMapReadyCallback {
     private GoogleMap mMap;
-    Button btn_back;
-
+    private ActivityMapsBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_maps);
+
+        binding = ActivityMapsBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
         // Obtain the SupportMapFragment and get notified when the map is ready to be used.
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
-
-        // set back_to_home button to go to home page
-//        btn_back = (Button) findViewById(R.id.back_to_home);
-//        btn_back.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View view) {
-//                startActivity(new Intent(VaccinationFinder.this, HomePage.class));
-//            }
-//        });
-
-//        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
-//                .findFragmentById(R.id.mapView);
-////        mapFragment.getMapAsync(this);
-//
-//        // Initialize Vaccination Provider Data
     }
 
     /**
@@ -55,7 +35,6 @@ public class VaccinationFinder extends AppCompatActivity implements OnMapReadyCa
      * This callback is triggered when the map is ready to be used.
      * This is where we can add markers or lines, add listeners or move the camera. In this case,
      * we just add a marker near Sydney, Australia.
-     *
      * If Google Play services is not installed on the device, the user will be prompted to install
      * it inside the SupportMapFragment. This method will only be triggered once the user has
      * installed Google Play services and returned to the app.
@@ -66,9 +45,7 @@ public class VaccinationFinder extends AppCompatActivity implements OnMapReadyCa
 
         // Add a marker in Sydney and move the camera
         LatLng sydney = new LatLng(-34, 151);
-        mMap.addMarker(new MarkerOptions()
-                .position(sydney)
-                .title("Marker in Sydney"));
+        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
         mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
     }
 }
